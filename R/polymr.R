f21032a894af8d0bfa5fc19bece497c46aa59afa
Jonathan Sulc <jonsulc@gmail.com>
2021-09-08 07:58:59 +0200

    Clean up PolyMR code and improve performance

--- R/polymr.R
+++ R/polymr.R
@@ -4,7 +4,7 @@
 #' @param y A vector containing the individual outcome values
 #' @param G The genetic matrix
 #' @param others Character vector containing other values to return.
-#'     'model' returns the polynomial approximation of the observed association,
+#'     'observational' returns the polynomial approximation of the observed association,
 #'     'offset' returns the scaling parameters of x and y,
 #'     'means' returns the mean value of y for each bin while 'medians' returns their median.
 #' @param bins Number of bins for which to return median or mean values
@@ -20,19 +20,19 @@
 #'
 #' @importFrom lmtest lrtest
 #' @importFrom meta metagen
-#' @importFrom stringr str_detect str_match str_replace
+#' @importFrom stringr str_detect str_match str_replace str_extract
 #'
 #' @export
 #'
 polymr  =  function( x,
                      y,
                      G,
-                     others  = c( 'model', 'means', 'offset' ),
+                     others  = c( 'observational' ),
                      bins = 100,
                      reverse_t = NULL,
                      ... ) {
-
-  if ('offset' %in% others) {
+  others  =  match.arg( others )
+  if ('observational' %in% others) {
     results  =  list( offset = data.frame( mean = c( mean(x), mean(y) ),
                                            sd   = c(   sd(x),   sd(y) ),
                                            row.names = c( 'x', 'y' )) )
@@ -43,12 +43,14 @@ polymr  =  function( x,
   x = scale(x)
   y = scale(y)
 
+  # Removing invariant IVs
   G  =  G[ , apply( G, 2, var ) != 0 ]
+
   bx  =  summary(lm( x~G ))$coefficients[ 1:ncol(G)+1, 1:2 ]
   betax  =  bx[ , 1 ]
-  sx  =  bx[ , 2 ]
 
   if (!is.null( reverse_t )) {
+    sx  =  bx[ , 2 ]
     betay  =  lm( y~G )
     if (any(is.na( betay$coefficients ))) {
       warning( 'Columns of G are not independent, i.e. some IVs have an LD of 1 in the provided sample, discarding duplicates.' )
@@ -63,7 +65,9 @@ polymr  =  function( x,
 
   epsX  =  x - G %*% betax
 
-  results$polymr  =  .polymr_wrapper( list( x = x, y = y, epsX = epsX ),
+  results$polymr  =  .polymr_wrapper( x = x,
+                                      y = y,
+                                      epsX = epsX,
                                       ... )
 
   if ('medians' %in% others) {
@@ -77,7 +81,7 @@ polymr  =  function( x,
     rownames( results$medians )  =  c( 'x', 'y_sd', 'y_median' )
   }
 
-  if ('means' %in% others) {
+  if ('means' %in% others | 'observational' %in% others) {
     results$means  =
       sapply( 1:bins, function( k ){
         bin  =  x > quantile( x, (k-1)/bins ) & x <= quantile( x, k/bins )
@@ -88,80 +92,105 @@ polymr  =  function( x,
     rownames( results$means )  =  c( 'x', 'y_sd', 'y_mean' )
   }
 
-  if ('model' %in% others) {
-    results$model  =  .model_wrapper( list( x = x, y = y ),
-                                      ... )
+  if ('observational' %in% others) {
+    results$observational  =  .model_wrapper( x = x,
+                                              y = y,
+                                              ... )
   }
 
   results
 }
 
-.polymr_wrapper  =  function( data_list,
+.polymr_wrapper  =  function( x,
+                              y,
+                              epsX,
+                              powers = 1:10,
+                              confounder_powers = 1:min(max(powers), max_confounder_power),
+                              max_confounder_power = NULL,
                               ... ) {
-  .loop( data_list,
+  dat  =  data.frame( y = y,
+                      x1 = x,
+                      epsX1 = epsX )
+  x_terms      =  paste0( 'x', powers )
+  dat  =  .create_dat( dat,
+                       c( x_terms, paste0( 'epsX', confounder_powers ) ) )
+
+  .loop( dat = dat,
+         powers = powers,
          method_function = .polymr_regression,
-         intercept_name = NULL,
          coef_names = 'alpha',
+         intercept_name = NULL,
+         max_confounder_power = max_confounder_power,
          ... )
 }
 
-.polymr_regression  =  function( data_list,
-                                 powers = 1:2,
-                                 max_confounder_power = max(powers),
-                                 confounder_powers = 1:min(max(powers), max_confounder_power),
-                                 ... ){
-  dat  =  cbind( data_list$y,
-                 outer( c(data_list$x), powers, '^' ),
-                 outer( c(data_list$epsX), confounder_powers, '^' ) )
-  dat  =  data.frame( dat )
-  colnames( dat )  =  c( 'y',
-                         paste0( 'x', powers ),
-                         paste0( 'epsX', confounder_powers ) )
-  model = lm( y ~ ., data = dat )
-  to_return  =  list( model = model )
-
-  if (!(1 %in% powers)) {
-    lrt1  =  lmtest::lrtest( model, lm( y ~ ., data = cbind( dat[ , -c( 1:length(powers)+1 ) ],
-                                                             data.frame( x1 = data_list$x ) ) ) )
-    pval1  =  .get_lrt_pval( lrt1 )
-
-    if (length( powers ) == 1) {
-      pval1  =  max( summary( model )$coefficients[ paste0( 'x', powers ), 'Pr(>|t|)' ],
-                     pval1 )
-    }
-  } else {
-    formula1  =  paste( '.~.', paste( '-x', setdiff( powers, 1 ), sep = '', collapse = '' ) )
-    pval1  =  .get_lrt_pval( lmtest::lrtest( model, update( model, as.formula( formula1 ) ) ) )
+.create_dat  =  function( dat,
+                          all_terms ){
+  new_terms  =  all_terms[ !(all_terms %in% colnames(dat)) ]
+  for (variable in unique( str_extract( new_terms, '^[a-zA-Z]+' ) )) {
+    powers  =  str_extract( new_terms[ str_detect( new_terms, paste0('^', variable, '[0-9]+$') ) ], '[0-9]+$' )
+    new_dat  =  data.frame( outer( dat[ , paste0(variable, 1) ],
+                                   as.numeric( powers ),
+                                   '^') )
+    colnames( new_dat )  =  paste0( variable, powers )
+    dat  =  cbind( dat, new_dat )
   }
-
-  formula0  =  as.formula( paste( '.~.', paste( '-x', powers, sep = '', collapse = '' ) ) )
-  model0  =  update( model, formula0 )
-
-  c( list( pval0 = .get_lrt_pval( lmtest::lrtest( model, model0 ) ),
-           pval1 = pval1,
-           r.squared = summary( model )$r.squared - summary( model0 )$r.squared ),
-     to_return )
+  dat
 }
 
-.get_lrt_pval  =  function( lrt,
-                            infer_dfs = TRUE ) {
-  if (infer_dfs) {
-    df1  =  stringr::str_match( attributes( lrt )$heading[2], 'x([0-9]+)[^x]*Model' )
-    df2  =  stringr::str_match( attributes( lrt )$heading[2], 'x([0-9]+)[^x]*$' )
-    dfs  =  c(df1[ , 2 ], df2[ , 2 ])
-    dfs  =  as.numeric( replace( dfs, is.na( dfs ), 0 ) )
-    dfs  =  dfs[1] - dfs[2]
-  } else {
-    dfs  =  lrt[ 2, 'Df' ]
+.get_formula  =  function( all_terms,
+                           remove = NULL,
+                           add = NULL,
+                           independent_variable = 'y' ){
+  if (!is.null( remove )) {
+    all_terms  =  all_terms[ !str_detect( all_terms, remove ) ]
   }
+
+  f = paste0( independent_variable, ' ~ ',
+              paste( c( add, all_terms ), collapse = ' + ' ) )
+  as.formula( f )
+}
+
+.polymr_regression  =  function( dat,
+                                 powers = 1:10,
+                                 x_terms = paste0( 'x', powers ),
+                                 other_terms = paste0( 'epsX', confounder_powers ),
+                                 max_confounder_power = NULL,
+                                 confounder_powers = 1:min(max(powers), max_confounder_power),
+                                 ... ){
+  if (any( !(c(x_terms, other_terms) %in% colnames(dat)) )) {
+    dat  =  .create_dat( dat, c(x_terms, other_terms) )
+  }
+  model = lm( .get_formula( c(x_terms, other_terms) ), dat )
+  to_return  =  list( model = model )
+
+  pval1  =  .get_lrt_pval( lmtest::lrtest( model,
+                                           update( model,
+                                                   .get_formula( c( 'x1', other_terms ) ),
+                                                   data = model$model ) ) )
+
+  model0  =  update( model,
+                     .get_formula( other_terms ),
+                     data = model$model )
+
+  list( pval0 = .get_lrt_pval( lmtest::lrtest( model, model0 ) ),
+        pval1 = pval1,
+        r.squared = summary( model )$r.squared - summary( model0 )$r.squared,
+        dat = dat,
+        model = model )
+}
+
+.get_lrt_pval  =  function( lrt ) {
   pchisq( 2 * ( lrt[ 1, 'LogLik' ] - lrt[ 2, 'LogLik' ] ),
-          abs( dfs ),
+          abs( lrt[[ '#Df' ]][ 1 ] - lrt[[ '#Df' ]][ 2 ] ),
           lower.tail = FALSE )
 }
 
-.loop  =  function( data_list,
-                    method_function = .polymr_regression,
+
+.loop  =  function( dat,
                     powers = 1:min(10, max_power),
+                    ...,
+                    method_function = .polymr_regression,
                     power_step = 2,
                     max_power = 10,
                     drop_lower = TRUE,
@@ -169,18 +198,18 @@ polymr  =  function( x,
                     p_thr_drop = .05/length( powers ),
                     return_vcov = TRUE,
                     coef_names = 'alpha',
-                    intercept_name = NULL,
-                    ... ){
-  temp_results  =  final_results  =  method_function( data_list,
+                    intercept_name = NULL ){
+  temp_results  =  final_results  =  method_function( dat = dat,
                                                       powers = powers,
                                                       ... )
-
-  while (min(summary( temp_results$model )$coefficients[ paste0( 'x', powers ), 'Pr(>|t|)' ]) < p_thr_add) {
+  selection_terms  =  paste0( 'x', powers )
+  while (min(summary( temp_results$model )$coefficients[ selection_terms, 'Pr(>|t|)' ]) < p_thr_add) {
     final_results  =  temp_results
     if (max(powers) < max_power) {
       new_powers  =  c(max(powers)+1:power_step)[ c(max(powers)+1:power_step) <= max_power ]
+      selection_terms  =  paste0( 'x', new_powers )
       powers  =  c( powers, new_powers )
-      temp_results  =  method_function( data_list,
+      temp_results  =  method_function( dat = dat,
                                         powers = powers,
                                         ... )
     } else {
@@ -189,42 +218,35 @@ polymr  =  function( x,
     }
   }
 
-  if (!is.null( final_results$powers )) {
-    powers  =  final_results$powers
-  } else {
-    powers  =  stringr::str_match( names( final_results$model$coefficients ), '^x([0-9]+)$' )
-    powers  =  powers[ !is.na(powers[,2]), 2 ]
-    powers  =  as.numeric( powers )
-  }
-  temp_results  =  final_results
-  pval  =  summary( final_results$model )$coefficients
-  pval  =  pval[ stringr::str_detect( rownames(pval), '^x[0-9]+' ), 'Pr(>|t|)' ]
+  powers  =  stringr::str_match( names( final_results$model$coefficients ), '^x([0-9]+)$' )
+  powers  =  powers[ !is.na(powers[,2]), 2 ]
+  powers  =  as.numeric( powers )
 
-  while (drop_lower & any(pval > p_thr_drop)) {
+  pvals  =  summary( final_results$model )$coefficients
+  pvals  =  pvals[ paste0( 'x', powers ), 'Pr(>|t|)' ]
+
+  while (drop_lower & any(pvals > p_thr_drop)) {
     if (length(powers) == 1) {
       return( c( list( coefficients = data.frame( estimate = numeric(), se = numeric() ) ),
                  final_results[ c( 'pval0', 'pval1' )[ c( 'pval0', 'pval1' ) %in% names( final_results ) ] ] ))
     }
 
-    powers  =  powers[ -which.max( pval ) ]
-    final_results  =  method_function( data_list,
+    powers  =  powers[ -which.max( pvals ) ]
+    final_results  =  method_function( dat = dat,
                                        powers = powers,
                                        ... )
-    pval  =  summary( final_results$model )$coefficients
-    pval  =  pval[ stringr::str_detect( rownames(pval), '^x[0-9]+' ), 'Pr(>|t|)' ]
+    pvals  =  summary( final_results$model )$coefficients
+    pvals  =  pvals[ paste0( 'x', powers ), 'Pr(>|t|)' ]
   }
 
   final_results$coefficients  =  summary( final_results$model )$coefficients
-  rownames( final_results$coefficients )  =  stringr::str_replace( rownames( final_results$coefficients ),
-                                                                   'x',
-                                                                   coef_names )
-  powers  =  stringr::str_match( names( final_results$model$coefficients ),
-                                 '^x([0-9]+)$' )[ , 2 ]
-  powers  =  as.numeric( powers[ !is.na(powers) ] )
 
-  final_results$coefficients  =  final_results$coefficients[ paste0( coef_names, powers ),
+  final_results$coefficients  =  final_results$coefficients[ paste0( 'x', powers ),
                                                              c('Estimate', 'Std. Error'),
                                                              drop = FALSE ]
+  rownames( final_results$coefficients )  =  stringr::str_replace( rownames( final_results$coefficients ),
+                                                                   '^x',
+                                                                   coef_names )
 
   colnames( final_results$coefficients )[ 1:2 ]  =  c( 'estimate', 'se' )
 
@@ -232,60 +254,47 @@ polymr  =  function( x,
     final_results$vcov  =  vcov( final_results$model )
     rownames( final_results$vcov )  =
       colnames( final_results$vcov )  =  stringr::str_replace( rownames( final_results$vcov ),
-                                                               'x',
+                                                               '^x',
                                                                coef_names )
   }
-  final_results$model = final_results$pval = final_results$powers = NULL
+  final_results$model = final_results$dat = NULL
 
   final_results
 }
 
-.model_wrapper  =  function( data_list,
-                             powers = 0:2,
+.model_wrapper  =  function( y,
+                             x,
+                             powers = 0:10,
                              ... ){
-  .loop( data_list,
-         method_function = .model_regression,
+  dat  =  data.frame( y = y,
+                      x1 = x )
+  dat  =  .create_dat( dat, paste0( 'x', powers ) )
+  .loop( dat = dat,
          powers = unique( c( 0, powers ) ),
+         method_function = .model_regression,
          coef_names = 'x',
          ... )
 }
 
-.model_regression  =  function( data_list,
+.model_regression  =  function( dat,
                                 powers = 0:2,
                                 null_model_powers = 0:1,
                                 ... ) {
-  dat  =  cbind( data_list$y,
-                 outer( c(data_list$x), powers, '^' ) )
-  dat  =  data.frame( dat )
-  colnames( dat )  =  c( 'y',
-                         paste0( 'x', powers ) )
   model = lm( y ~ .-1, data = dat )
   to_return  =  list( model = model )
 
   if (is.null( null_model_powers )) {
-    return( c( to_return, list( pval = NULL ) ) )
-  }
-
-  if (!all(null_model_powers %in% powers)) {
-    pval1  =  lmtest::lrtest( model,
-                      lm( y ~ .-1, data = cbind( dat[ , 'y', drop = FALSE ],
-                                                 outer( c(data_list$x), null_model_powers, '^' ) ) ) )
-    pval1  =  .get_lrt_pval( pval1 )
-    if (length( powers ) == length( null_model_powers ) & length( powers ) == 1) {
-      pval1  =  max( summary( model )$coefficients[ paste0( 'x', powers ), 'Pr(>|t|)' ],
-                     pval1 )
-    }
+    pval1 = NULL
   } else {
-    formula1  =  as.formula( paste( '.~.', paste( '-x',
-                                                  setdiff( powers, null_model_powers ),
-                                                  sep = '',
-                                                  collapse = '' ) ) )
-    pval1  =  .get_lrt_pval( lmtest::lrtest( model, update( model, formula1 ) ) )
+    pval1  =  lmtest::lrtest( model,
+                              lm( .get_formula(paste0('x', null_model_powers)),
+                                  data = model$model ) )
+    pval1  =  .get_lrt_pval( pval1 )
   }
 
-  c( to_return,
-     list( pval1 = pval1,
-           r.squared = summary( model )$r.squared ) )
+  list( model = model,
+        pval1 = pval1,
+        r.squared = summary( model )$r.squared )
 }
 
 
